d9656cb64e6b71b2555a8e7112857fb639578781
Muhammad Surya Jayadiprana <50776952+suryamsj@users.noreply.github.com>
2021-07-30 21:41:43 +0700

    Push pertama kali
    Melakukan push untuk pertama kali ke repo

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Botulis
- Botulis adalah website untuk menjalankan bot yang bisa menulis
+<div align="center" id="top"> 
+  <img src="./screenshot/botulis.jpeg" alt="Botulis" />
+  &#xa0;
+</div>
+
+<h1 align="center">Botulis</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/suryamsj/botulis?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/suryamsj/botulis?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/suryamsj/botulis?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/suryamsj/botulis?color=56BEB8">
+
+  <img alt="Github forks" src="https://img.shields.io/github/forks/suryamsj/botulis?color=56BEB8" />
+
+  <img alt="Github stars" src="https://img.shields.io/github/stars/suryamsj/botulis?color=56BEB8" />
+</p>
+
+<p align="center">
+  <a href="#botulis-tentang">Tentang</a> &#xa0; | &#xa0; 
+  <a href="#botulis-teknologi">Teknologi</a> &#xa0; | &#xa0;
+  <a href="#botulis-lisensi">Lisensi</a> &#xa0; | &#xa0;
+  <a href="https://github.com/suryamsj" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :botulis: Tentang ##
+
+Botulis adalah website untuk menjalankan bot yang bisa menulis dengan maksud untuk membantu kamu dalam menyalin / mencatat agar lebih cepat.
+
+## :botulis: Teknologi ##
+
+Project ini menggunakan:
+
+- [PHP V7.4.14](https://www.php.net/)
+- [Bulma](https://bulma.io/)
+- [Sweetalert 2](https://sweetalert2.github.io/)
+- [Ihsandevs API](https://backend-ihsandevs.herokuapp.com/api/)
+
+## :botulis: Lisensi ##
+
+Project ini dibawah lisensi MIT. untuk lebih jelas, bisa lihat [LICENSE](LICENSE.md).
+
+
+Dibuat dengan :heart: oleh <a href="https://github.com/suryamsj" target="_blank">Muhammad Surya Jayadiprana</a>
+
+&#xa0;
+
+<a href="#top">Kembali ke atas</a>
---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Botulis</title>
+    <meta name="description" content="Botulis adalah website untuk menjalankan bot yang bisa menulis">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="asset/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body>
+    <!-- Tombol klik -->
+    <?php
+    if (isset($_POST["kirim"])) {
+        $teks = $_POST["tulisan"];
+
+        // Cek kalo teks tidak kosong
+        if (!empty($teks)) {
+            $url = "http://backend-ihsandevs.herokuapp.com/api/Nulis/?text=" . urlencode($teks);
+            $data = json_decode(file_get_contents($url), true);
+            $status = $data["status"];
+            $message = $data["message"];
+            $result = $data["result"];
+        }
+        // Cek kalo teks kosong
+        else {
+            echo "
+            <script>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Lengkapi form',
+                    text: 'Form tidak boleh kosong!'
+                })
+            </script>
+            ";
+        }
+    }
+    ?>
+
+    <!-- Start Navbar -->
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <img src="asset/img/logo.png" width="120" height="36">
+                </a>
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+
+    <!-- Start Main -->
+    <main class="is-flex-shrink-0">
+
+        <!-- Start Header -->
+        <header class="hero is-medium is-link">
+            <div class="hero-body">
+                <p class="title">
+                    BOTULIS
+                </p>
+                <p class="subtitle">
+                    Kamu cape nulis? Tenang, tinggal copy text dan biarkan bot menuliskannya untuk mu.
+                </p>
+            </div>
+        </header>
+        <!-- End Header -->
+
+        <section class="mt-6 mb-6">
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-6">
+                        <div class="card">
+                            <header class="card-header">
+                                <p class="card-header-title">
+                                    Form Tulisan
+                                </p>
+                                <button class="card-header-icon" aria-label="more options">
+                                    <span class="icon">
+                                        <i class="far fa-paper-plane" aria-hidden="true"></i>
+                                    </span>
+                                </button>
+                            </header>
+                            <div class="card-content">
+                                <div class="content">
+                                    <form action="" method="post">
+                                        <div class="field">
+                                            <label class="label">Teks</label>
+                                            <div class="control">
+                                                <textarea class="textarea" placeholder="Masukkan teks yang akan ditulis.." name="tulisan" required></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="field is-grouped">
+                                            <div class="control">
+                                                <button class="button is-link" type="submit" name="kirim">Kirim</button>
+                                            </div>
+                                            <div class="control">
+                                                <button class="button is-link is-light" type="reset">Ulang</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-6">
+                        <div class="card">
+                            <header class="card-header">
+                                <p class="card-header-title">
+                                    Gambar
+                                </p>
+                                <button class="card-header-icon" aria-label="more options">
+                                    <span class="icon">
+                                        <i class="far fa-images" aria-hidden="true"></i>
+                                    </span>
+                                </button>
+                            </header>
+                            <div class="card-content">
+                                <div class="content">
+                                    <?php if (!empty($result)) : ?>
+                                        <figure class="image is-square">
+                                            <img src="<?= $result['path'] ?>" alt="gambar bot" id="gambar_bot">
+                                        </figure>
+                                        <a class="button is-link" href="<?= $result['path'] ?>" download="<?= $result['path'] ?>" target="_blank">Download</a>
+                                    <?php else : ?>
+                                        <figure class="image is-square">
+                                            <img src="asset/img/no-data-animate.svg" alt="gak ada gambar">
+                                        </figure>
+                                    <?php endif ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+    <!-- End Main -->
+
+    <!-- Start Footer -->
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>Botulis dibuat dengan <span class="has-text-danger">&hearts;</span> oleh <strong><a href="https://suryamsj.my.id/">Muhammad Surya J</a></strong></p>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <!-- Cek Status -->
+    <?php if (!empty($status)) : ?>
+        <?php if ($status == 0) : ?>
+            <script>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Gagal',
+                    text: 'Bot gagal menulis!'
+                })
+            </script>
+        <?php elseif ($status == 1) : ?>
+            <script>
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Berhasil',
+                    text: 'Bot berhasil menulis!'
+                })
+            </script>
+        <?php endif ?>
+    <?php endif ?>
+    <script src="asset/js/main.js"></script>
+</body>
+
+</html>
